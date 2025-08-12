817c012587e1d8f15ef8f36f0e1defd3850c9a7b
sztynksior <szszedel@gmail.com>
2025-08-12 15:15:09 +0200

    implement one point crossover

--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 #include <iostream>
 
 class Chromosome {
-    std::list<int> genes;
 public: 
+    std::list<int> genes;
+
     Chromosome(std::list<int> g) : genes(std::move(g)) {};
+
     void print() const {
         for (const auto& gene : genes) {
             std::cout << gene << " ";
@@ -13,8 +15,31 @@ public:
     }
 };
 
+class Crossover {
+public:
+    Crossover() = default;
+    void performCrossover(std::list<int>& parent1, std::list<int>& parent2) {
+        int crossoverPoint = std::rand() % parent1.size();
+        crossoverPoint = std::max(1, crossoverPoint);
+        auto firstParentCorssPoint = std::next(parent1.begin(), crossoverPoint);
+        auto secondParentCrossPoint = std::next(parent2.begin(), crossoverPoint);
+        auto spliceSize = parent1.size() - crossoverPoint;
+
+        parent1.splice(parent1.end(), parent2, secondParentCrossPoint, parent2.end());
+        parent2.splice(parent2.end(), parent1, firstParentCorssPoint, std::prev(parent1.end(), spliceSize));
+    }
+};
+
 int main() {
-    Chromosome chromo({1, 0, 1, 1, 0});
-    chromo.print();
+    srand(time(0));
+
+    Crossover crossover;
+    Chromosome parent1({11, 22, 33, 44, 55});
+    Chromosome parent2({1, 2, 3, 4, 5});
+
+    crossover.performCrossover(parent1.genes, parent2.genes);
+    parent1.print();
+    parent2.print();
+
     return 0;
 }

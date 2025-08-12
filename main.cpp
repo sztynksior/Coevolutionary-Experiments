b675e332323750cda49ccc84db11ec77d675a830
sztynksior <szszedel@gmail.com>
2025-08-12 15:34:27 +0200

    added abstraction to the Crossover

--- main.cpp
+++ main.cpp
@@ -1,24 +1,15 @@
 #include <list>
 #include <iostream>
 
-class Chromosome {
-public: 
-    std::list<int> genes;
-
-    Chromosome(std::list<int> g) : genes(std::move(g)) {};
-
-    void print() const {
-        for (const auto& gene : genes) {
-            std::cout << gene << " ";
-        }
-        std::cout << std::endl;
-    }
-};
-
 class Crossover {
 public:
-    Crossover() = default;
-    void performCrossover(std::list<int>& parent1, std::list<int>& parent2) {
+    virtual void performCrossover(std::list<int>& parent1, std::list<int>& parent2) = 0;
+};
+
+class OnePointCrossover : public Crossover {
+    public:
+    OnePointCrossover() = default;
+    void performCrossover(std::list<int>& parent1, std::list<int>& parent2) override {
         int crossoverPoint = std::rand() % parent1.size();
         crossoverPoint = std::max(1, crossoverPoint);
         auto firstParentCorssPoint = std::next(parent1.begin(), crossoverPoint);
@@ -30,14 +21,36 @@ public:
     }
 };
 
+class Chromosome {
+    std::list<int> genes;
+    Crossover* crossover;
+
+    public: 
+
+    Chromosome(
+        std::list<int> g, Crossover* c
+    ) : genes(std::move(g)), crossover(c) {};
+
+    void print() const {
+        for (const auto& gene : genes) {
+            std::cout << gene << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    void performCrossover(Chromosome& other) {
+        crossover->performCrossover(genes, other.genes);
+    }
+};
+
 int main() {
     srand(time(0));
 
-    Crossover crossover;
-    Chromosome parent1({11, 22, 33, 44, 55});
-    Chromosome parent2({1, 2, 3, 4, 5});
+    Crossover* crossover = new OnePointCrossover();
+    Chromosome parent1({11, 22, 33, 44, 55}, crossover);
+    Chromosome parent2({1, 2, 3, 4, 5}, crossover);
 
-    crossover.performCrossover(parent1.genes, parent2.genes);
+    parent1.performCrossover(parent2);
     parent1.print();
     parent2.print();
 
